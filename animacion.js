8d0c4924999e6be4b818f20bf6e6197c7db9e1f1
martubecerra <2214822@ucc.edu.ar>
2023-06-05 20:13:42 -0300

    canvas

--- animacion.js
+++ animacion.js
@@ -1,20 +1,84 @@
-function dibujar(){
+function dibujar() {
     const canvas = document.getElementById("myCanvas");
     const ctx = canvas.getContext("2d");
 
-    let img = new Image();
-    img.src = "imagenes/marte.png";
+    canvas.width = canvas.width;
 
-    img.onload = function () {
-        const escala = 0.3;
-        let ancho = img.width * escala;
-        let alto = img.height * escala;
-        ctx.drawImage(img, 0, 35, ancho, alto);
+    let img = new Image();
+
+    let gravedadSelect = document.getElementById('gravedad');
+
+    let planeta = gravedadSelect.value;
+
+    switch (planeta) {
+        case 'tierra':
+            img.src = "imagenes/tierra.png";
+            img.onload = function () {
+                const escala = 0.6;
+                let ancho = img.width * escala;
+                let alto = img.height * escala;
+                ctx.drawImage(img, 0, 35, ancho, alto);
+            }
+            break;
+        case 'marte':
+            img.src = "imagenes/marte.png";
+            img.onload = function () {
+                const escala = 0.3;
+                let ancho = img.width * escala;
+                let alto = img.height * escala;
+                ctx.drawImage(img, 0, 35, ancho, alto);
+            }
+            break;
+        case 'luna':
+            img.src = "imagenes/luna.png";
+            img.onload = function () {
+                const escala = 0.3;
+                let ancho = img.width * escala;
+                let alto = img.height * escala;
+                ctx.drawImage(img, 0, 35, ancho, alto);
+            }
+            break;
+        case 'mercurio':
+            img.src = "imagenes/mercurio.png";
+            img.onload = function () {
+                const escala = 0.3;
+                let ancho = img.width * escala;
+                let alto = img.height * escala;
+                ctx.drawImage(img, 0, 35, ancho, alto);
+            }
+            break;
+        case 'jupiter':
+            img.src = "imagenes/jupiter.png";
+            img.onload = function () {
+                const escala = 0.3;
+                let ancho = img.width * escala;
+                let alto = img.height * escala;
+                ctx.drawImage(img, 0, 35, ancho, alto);
+            }
+            break;
+        case 'saturno':
+            img.src = "imagenes/saturno.png";
+            img.onload = function () {
+                const escala = 0.3;
+                let ancho = img.width * escala;
+                let alto = img.height * escala;
+                ctx.drawImage(img, 0, 35, ancho, alto);
+            }
+            break;
+        case 'neptuno':
+            img.src = "imagenes/neptuno.png";
+            img.onload = function () {
+                const escala = 0.3;
+                let ancho = img.width * escala;
+                let alto = img.height * escala;
+                ctx.drawImage(img, 0, 35, ancho, alto);
+            }
+            break;
     }
 }
-/*
-x=0;
-dx=4;
+
+/*let x=0;
+const dx=4;
 function mover() {
     const canvas = document.getElementById("myCanvas");
     const ctx = canvas.getContext("2d");
@@ -22,13 +86,75 @@ function mover() {
     canvas.width = canvas.width;
 
     let img = new Image();
-    img.src = "imagenes/marte.png";
 
-    img.onload = function () {
-        const escala = 0.3;
-        let ancho = img.width * escala;
-        let alto = img.height * escala;
-        ctx.drawImage(img, x, 35, ancho, alto);
+    let gravedadSelect = document.getElementById('gravedad');
+
+    let planeta = gravedadSelect.value;
+
+    switch (planeta) {
+        case 'tierra':
+            img.src = "imagenes/tierra.png";
+            img.onload = function () {
+                const escala = 0.6;
+                let ancho = img.width * escala;
+                let alto = img.height * escala;
+                ctx.drawImage(img, x, 35, ancho, alto);
+            }
+            break;
+        case 'marte':
+            img.src = "imagenes/marte.png";
+            img.onload = function () {
+                const escala = 0.3;
+                let ancho = img.width * escala;
+                let alto = img.height * escala;
+                ctx.drawImage(img, x, 35, ancho, alto);
+            }
+            break;
+        case 'luna':
+            img.src = "imagenes/luna.png";
+            img.onload = function () {
+                const escala = 0.3;
+                let ancho = img.width * escala;
+                let alto = img.height * escala;
+                ctx.drawImage(img, x, 35, ancho, alto);
+            }
+            break;
+        case 'mercurio':
+            img.src = "imagenes/mercurio.png";
+            img.onload = function () {
+                const escala = 0.3;
+                let ancho = img.width * escala;
+                let alto = img.height * escala;
+                ctx.drawImage(img, x, 35, ancho, alto);
+            }
+            break;
+        case 'jupiter':
+            img.src = "imagenes/jupiter.png";
+            img.onload = function () {
+                const escala = 0.3;
+                let ancho = img.width * escala;
+                let alto = img.height * escala;
+                ctx.drawImage(img, x, 35, ancho, alto);
+            }
+            break;
+        case 'saturno':
+            img.src = "imagenes/saturno.png";
+            img.onload = function () {
+                const escala = 0.3;
+                let ancho = img.width * escala;
+                let alto = img.height * escala;
+                ctx.drawImage(img, x, 35, ancho, alto);
+            }
+            break;
+        case 'neptuno':
+            img.src = "imagenes/neptuno.png";
+            img.onload = function () {
+                const escala = 0.3;
+                let ancho = img.width * escala;
+                let alto = img.height * escala;
+                ctx.drawImage(img, x, 35, ancho, alto);
+            }
+            break;
     }
 
     if (x > canvas.width) {
